25e1b57105c7864f6844c9d8647fc8a8a10ad707
Kai <kai.merkel@innoversa-factory.com>
2026-07-10 08:49:39 +0000

    Add task 7: Concatenate strings

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -5,7 +5,7 @@ class Program
     {
         string str1 = "Holberton";
         string str2 = "School";
-        string result = "Welcome to "+str1 + " " + str2;
+        string result = "Welcome to "+str1 + " " + str2+"!";
         Console.WriteLine(result);
     }
 }
