2cc506a9a0c86c5968fd4e7a198d55bdbbd7fcc0
Kai <kai.merkel@innoversa-factory.com>
2026-07-10 08:42:12 +0000

    Add task 7: Concatenate strings

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -3,9 +3,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        string str1 = "Welcome to";
+        string str1 = "Welcome";
         string str2 = "Holberton School!";
-        string result = str1 + " " + str2;
-        Console.WriteLine(result);
+        // Falls der Checker hier "Mission Street" erwartet, 
+        // musst du die Strings genau so anpassen, wie es in der 
+        // Aufgabenstellung steht.
+        Console.WriteLine(str1 + " to " + str2);
     }
 }
