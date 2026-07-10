00b6362651dc313e29046dcac20c6f5550865f48
Kai <kai.merkel@innoversa-factory.com>
2026-07-10 11:25:41 +0000

    Add task 6: Print all unique two-digit combinations

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                if (i == 8 && j == 9)
+                    Console.Write($"{i}{j}\n");
+                else
+                    Console.Write($"{i}{j}, ");
+            }
+        }
+    }
+}
