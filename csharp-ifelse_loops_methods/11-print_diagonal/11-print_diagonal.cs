22eaa9451c99ff011ab9c3962087ea667f703f91
Kai <kai.merkel@innoversa-factory.com>
2026-07-10 12:36:40 +0000

    Add task 11: Print a diagonal line of backslashes

--- csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -4,23 +4,19 @@ public class Line
 {
     public static void PrintDiagonal(int length)
     {
-        if (length > 0)
+        if (length <= 0)
         {
-            for (int i = 0; i < length; i++)
-            {
-                // Drucke i Leerzeichen
-                for (int j = 0; j < i; j++)
-                {
-                    Console.Write(" ");
-                }
-                // Drucke den Backslash
-                Console.WriteLine("\\");
-            }
-        }
-        else
-        {
-            // Bei length <= 0 nur eine leere Zeile
             Console.WriteLine();
+            return;
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
         }
     }
 }
