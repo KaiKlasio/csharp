04219250a913319b4091550e54aeb82652ca7f2f
Kai <kai.merkel@innoversa-factory.com>
2026-07-10 08:53:43 +0000

    Add task 9: Print variables with specific format specifiers

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = .7553;
+        double currency = 98765.4321;
+        Console.WriteLine($"Percent: {percent:P2} tip");
+        Console.WriteLine($"Currency: {currency.ToString("C2", new CultureInfo("en-US"))}");
+    }
+}
