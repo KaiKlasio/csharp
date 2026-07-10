a9bec33f312b0b740b3d740f4d4173968cb05fe1
Kai <kai.merkel@innoversa-factory.com>
2026-07-10 12:17:38 +0000

    Add task 8: Method to print and return the last digit of a number

--- csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -5,7 +5,7 @@ public class Number
     public static int PrintLastDigit(int number)
     {
         int lastDigit = Math.Abs(number % 10);
-        Console.Write(lastDigit);
+        //Console.Write(lastDigit);
         return lastDigit;
     }
 }
