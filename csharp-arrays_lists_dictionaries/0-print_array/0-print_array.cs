7b6dd77d5cc919afed7c1ad57bc07e92563d1824
Kai <kai.merkel@innoversa-factory.com>
2026-07-10 12:43:55 +0000

    Add task 0: Print array of integers of a given size

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+
+        int[] myArray = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            myArray[i] = i;
+            Console.Write(myArray[i] + (i == size - 1 ? "" : " "));
+        }
+        Console.WriteLine();
+
+        return myArray;
+    }
+}
