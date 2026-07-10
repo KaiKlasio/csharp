3bccfab08f74f7b0e043624baf4127092ace58d9
Kai <kai.merkel@innoversa-factory.com>
2026-07-10 11:24:04 +0000

    Add task 5: Print numbers from 00 to 99 separated by comma

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (i < 99)
+                Console.Write($"{i:D2}, ");
+            else
+                Console.Write($"{i:D2}\n");
+        }
+    }
+}
