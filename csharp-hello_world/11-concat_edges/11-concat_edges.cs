bb146cd3b122d52ede6503947144a6bdeccaee57
Kai <kai.merkel@innoversa-factory.com>
2026-07-10 09:28:11 +0000

    Add task 11: Create sentence from existing string parts

---
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,10 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+        // Wir suchen die Wörter direkt im String, statt Indizes zu raten
+        Console.WriteLine(str.Substring(str.IndexOf("object-oriented"), 15) + " " + str.Substring(str.IndexOf("programming"), 11) + " in " + str.Substring(0, 2));
+    }
+}
